60bde43885516c47668d84817bea59c9242d5882
bogdangill <herecomesthefog@gmail.com>
2021-02-19 14:34:25 +0500

    подключение скриптов для блоков + сценарий

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,18 @@
 'use strict';
 
 import './config/jqueryLoad';
+import '../blocks/components/burger/burger.js';
+import '../blocks/components/main-nav/main-nav.js';
 import '@fancyapps/fancybox/dist/jquery.fancybox.min';
 import '../blocks/components/dropdown/dropdown.js';
+
+// Если js включен в браузере
+
+var tagHtml = document.querySelector('html');
+tagHtml.classList.add('js');
+
+// Если js включен в браузере
+
 // Плавный скол с навигации
 
 /* $(".scrollto > a").click(function () {
